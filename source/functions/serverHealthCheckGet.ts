55f992f70b98edae90b93d627e7cde545c28b9c6
Larry <lauris.skraucis@gmail.com>
2021-10-03 00:06:53 +0200

    Update get health check test

--- source/functions/serverHealthCheckGet.ts
+++ source/functions/serverHealthCheckGet.ts
@@ -4,7 +4,7 @@ import { buildResponse } from '../helpers/build-response';
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     let response;
     try {
-        response = buildResponse(200, 'Pong pong');
+        response = buildResponse(200, 'Pong');
     } catch (err) {
         response = buildResponse(500, 'An error occured');
     }
