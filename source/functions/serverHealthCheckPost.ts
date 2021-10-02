bcaaaf587340c6aed4a53922c0a44a54d6fe3cb7
Larry <lauris.skraucis@gmail.com>
2021-10-03 00:07:32 +0200

    Use response builder for post health check

--- source/functions/serverHealthCheckPost.ts
+++ source/functions/serverHealthCheckPost.ts
@@ -1,17 +1,14 @@
 import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { buildResponse } from '../helpers/build-response';
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    let response;
     try {
         const eventBody = event.body;
         const parsedBody = await JSON.parse(eventBody || '');
-        return {
-            statusCode: 200,
-            body: `Pong ${parsedBody}`
-        };
+        response = buildResponse(200, `${parsedBody.message} Pong`);
     } catch (err) {
-        return {
-            statusCode: 500,
-            body: 'An error occured'
-        };
+        response = buildResponse(500, 'An error occured');
     }
+    return response;
 };
