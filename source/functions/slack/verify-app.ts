50c7b4956ccb4801b7ca860db9385b49e277c126
Larry <lauris.skraucis@gmail.com>
2021-10-04 18:54:32 +0200

    Add slack app verification endpoint

---
+++ source/functions/slack/verify-app.ts
@@ -0,0 +1,14 @@
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { buildResponse } from '../../helpers/build-response';
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    let response;
+    try {
+        const eventBody = event.body;
+        const parsedBody = await JSON.parse(eventBody || '');
+        response = buildResponse(200, `${parsedBody.challenge}`);
+    } catch (err) {
+        response = buildResponse(500, 'An error occured');
+    }
+    return response;
+};
