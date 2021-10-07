9c3617d63fe933b1a8c73fb24b519d880c212d41
Larry <lauris.skraucis@gmail.com>
2021-10-07 19:03:39 +0200

    Implement good logging

--- source/functions/slack/eventsController.ts
+++ source/functions/slack/eventsController.ts
@@ -1,5 +1,10 @@
+import Logger from '../../services/logger';
+
+const NAMESPACE = 'eventsController';
+
 export default function processRequest(body: any): void {
     const type = body.event.type;
+    Logger.info(NAMESPACE, `Start processing request of type: ${type}`);
     if (type === 'message') {
         console.log('call prcoessMessages');
         prcoessMessages(body);
@@ -7,5 +12,6 @@ export default function processRequest(body: any): void {
 }
 
 function prcoessMessages(body: any) {
+    Logger.info(NAMESPACE, 'Start processing messages request');
     console.log('message', body.event.text);
 }
--- source/functions/slack/eventsRoute.ts
+++ source/functions/slack/eventsRoute.ts
@@ -3,30 +3,38 @@ import { buildResponse } from '../../helpers/build-response';
 import verifySlackEvent from './verify';
 import { Context, Callback } from 'aws-lambda';
 import processRequest from './eventsController';
+import Logger from '../../services/logger';
+
+const NAMESPACE = 'eventsRoute';
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent, context: Context, callback: Callback): Promise<APIGatewayProxyResult> => {
     let response;
     try {
+        Logger.info(NAMESPACE, 'Event received');
         if (verifySlackEvent(event)) {
+            Logger.info(NAMESPACE, 'Event successfully verified as Slack event');
             const eventBody = event.body;
             const parsedBody = await JSON.parse(eventBody || '');
             // This is special way how Slack verifies the app
-            console.log('body', parsedBody);
             if (parsedBody.type === 'url_verification') {
+                Logger.info(NAMESPACE, 'Event is of type: url_verification');
                 response = buildResponse(200, `${parsedBody.challenge}`);
             } else if (parsedBody.type === 'event_callback') {
+                Logger.info(NAMESPACE, 'Event is of type: event_callback');
                 // Call controller processing actual event
-                console.log('call processRequest');
                 processRequest(parsedBody.body);
                 response = buildResponse(200, { message: 'OK' });
             } else {
                 response = buildResponse(404, { message: 'Invalid request' });
+                Logger.info(NAMESPACE, `Event is of unknown type: ${parsedBody.type}`);
             }
         } else {
             response = buildResponse(401, { message: 'Unauthorized' });
+            Logger.info(NAMESPACE, 'Event is unauthorized. It is not sent by Slack.');
         }
     } catch (err) {
         response = buildResponse(500, { message: 'An error occured' });
+        Logger.error(NAMESPACE, 'An error occured: ', err.message);
     }
     return response;
 };
