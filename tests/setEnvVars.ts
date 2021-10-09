90278d87d36476bc509e9065be73dd01ceb04fa5
Larry <lauris.skraucis@gmail.com>
2021-10-09 13:05:29 +0200

    Set up testing environment variables

---
+++ tests/setEnvVars.ts
@@ -0,0 +1,7 @@
+// This file is used by jest.config.js to set up environment variables
+// within tests. Usually, you would set up environment variables as follows:
+// URL=https://google.com
+// but here you have to prepend process.env in front of it for it to work in tests:
+// process.env.URL=https://google.com
+
+process.env.EXAMPLE_ENVIRONMENT_VARIABLE = 'All tests green. Easy.';
