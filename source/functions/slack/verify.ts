96e022b2e9af1cba1118da44d05b4d3b2e737f59
Larry <lauris.skraucis@gmail.com>
2021-10-09 09:45:52 +0200

    Debug verifier

--- source/functions/slack/verify.ts
+++ source/functions/slack/verify.ts
@@ -11,17 +11,23 @@ const DIGEST_ENCODING = 'hex';
 function verifySlackEvent(slackEvent: APIGatewayProxyEvent, signingSecret = `${process.env.SLACK_SIGNING_SECRET}`, signatureVersion = 'v0'): boolean {
     // Validate Slack event
     Logger.debugNestedObject(NAMESPACE, 'Received event:', slackEvent);
-    Logger.info(NAMESPACE, `Environment signing secret: ${signingSecret}`);
     if (!slackEvent || !slackEvent.body || !slackEvent.headers || !slackEvent.headers['x-slack-request-timestamp'] || !slackEvent.headers['x-slack-signature']) {
         return false;
     }
     // Create expected signature
     const body = slackEvent.body;
+    Logger.debugNestedObject(NAMESPACE, 'Event body', body);
     const headers = makeObjectKeysValuesLowerCase(slackEvent.headers);
+    Logger.debugNestedObject(NAMESPACE, 'Event headers lowercased', headers);
     const timestamp = `${headers['x-slack-request-timestamp']}`;
+    Logger.debug(NAMESPACE, `timestamp: ${timestamp}`);
     const baseString = `${signatureVersion}:${timestamp}:${body}`;
+    Logger.debug(NAMESPACE, `baseString: ${timestamp}`);
     const hmac = crypto.createHmac(SHA_VERSION, signingSecret).update(baseString).digest(DIGEST_ENCODING);
+    Logger.debug(NAMESPACE, `hmac: ${timestamp}`);
     const exptectedSignature = `${signatureVersion}=${hmac}`;
+    Logger.debug(NAMESPACE, `exptectedSignature: ${exptectedSignature}`);
+    Logger.debug(NAMESPACE, `x-slack-signature: ${headers['x-slack-signature']}`);
     // Compare with received signature
     return exptectedSignature === headers['x-slack-signature'];
 }
