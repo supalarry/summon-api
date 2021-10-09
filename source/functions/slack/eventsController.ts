d30e53565f83adc3484fca69dadbe43fcb936a48
Larry <lauris.skraucis@gmail.com>
2021-10-09 10:58:01 +0200

    Improve logs of eventsController

--- source/functions/slack/eventsController.ts
+++ source/functions/slack/eventsController.ts
@@ -6,12 +6,10 @@ export default function processRequest(body: any): void {
     const type = body.event.type;
     Logger.info(NAMESPACE, `Start processing request of type: ${type}`);
     if (type === 'message') {
-        console.log('call prcoessMessages');
         prcoessMessages(body);
     }
 }
 
 function prcoessMessages(body: any) {
-    Logger.info(NAMESPACE, 'Start processing messages request');
-    console.log('message', body.event.text);
+    Logger.info(NAMESPACE, `prcoessMessages invoked to process message: ${body.event.text}`);
 }
