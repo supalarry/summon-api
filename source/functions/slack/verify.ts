53b987b0816604b13b7185277aa711b9baa9b8e7
Larry <lauris.skraucis@gmail.com>
2021-10-09 10:14:42 +0200

    Reformat verifier

--- source/functions/slack/verify.ts
+++ source/functions/slack/verify.ts
@@ -8,18 +8,24 @@ const NAMESPACE = 'verify';
 const SHA_VERSION = 'sha256';
 const DIGEST_ENCODING = 'hex';
 
+const SLACK_TIMESTAMP_HEADER_LOWERCASE = 'x-slack-request-timestamp';
+const SLACK_SIGNATURE_HEADER_LOWERCASE = 'x-slack-signature';
+
 function verifySlackEvent(slackEvent: APIGatewayProxyEvent, signingSecret = `${process.env.SLACK_SIGNING_SECRET}`, signatureVersion = 'v0'): boolean {
     // Validate Slack event
     Logger.debugNestedObject(NAMESPACE, 'Received event:', slackEvent);
-    if (!slackEvent || !slackEvent.body || !slackEvent.headers || !slackEvent.headers['x-slack-request-timestamp'] || !slackEvent.headers['x-slack-signature']) {
+    if (!slackEvent || !slackEvent.body || !slackEvent.headers) {
+        return false;
+    }
+    const headers = makeObjectKeysValuesLowerCase(slackEvent.headers);
+    if (!headers[SLACK_TIMESTAMP_HEADER_LOWERCASE] || !headers[SLACK_SIGNATURE_HEADER_LOWERCASE]) {
         return false;
     }
     // Create expected signature
     const body = slackEvent.body;
     Logger.debugNestedObject(NAMESPACE, 'Event body', body);
-    const headers = makeObjectKeysValuesLowerCase(slackEvent.headers);
     Logger.debugNestedObject(NAMESPACE, 'Event headers lowercased', headers);
-    const timestamp = `${headers['x-slack-request-timestamp']}`;
+    const timestamp = `${headers[SLACK_TIMESTAMP_HEADER_LOWERCASE]}`;
     Logger.debug(NAMESPACE, `timestamp: ${timestamp}`);
     const baseString = `${signatureVersion}:${timestamp}:${body}`;
     Logger.debug(NAMESPACE, `baseString: ${timestamp}`);
@@ -27,8 +33,8 @@ function verifySlackEvent(slackEvent: APIGatewayProxyEvent, signingSecret = `${p
     Logger.debug(NAMESPACE, `hmac: ${timestamp}`);
     const exptectedSignature = `${signatureVersion}=${hmac}`;
     Logger.debug(NAMESPACE, `exptectedSignature: ${exptectedSignature}`);
-    Logger.debug(NAMESPACE, `x-slack-signature: ${headers['x-slack-signature']}`);
+    Logger.debug(NAMESPACE, `x-slack-signature: ${headers[SLACK_SIGNATURE_HEADER_LOWERCASE]}`);
     // Compare with received signature
-    return exptectedSignature === headers['x-slack-signature'];
+    return exptectedSignature === headers[SLACK_SIGNATURE_HEADER_LOWERCASE];
 }
 export default verifySlackEvent;
